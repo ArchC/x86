fe6d979c6cc49cbaff02e0bd10e91951da2fe284
Bruno Albertini <balbertini@gmail.com>
2006-06-29 17:58:42 +0000

    Fixed header null brief, file and date (doxygen).

--- IA32-isa.cpp
+++ IA32-isa.cpp
@@ -18,7 +18,7 @@
  *            IC-UNICAMP
  *            http://www.lsc.ic.unicamp.br
  *
- * @version   version?
+ * @version   1.0
  * @date      Mon, 19 Jun 2006 15:33:29 -0300
  * 
  * @brief     The ArchC x86 functional model.
